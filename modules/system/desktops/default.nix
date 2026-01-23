0523df8bcf4d6498dfa6660bdb82df5e6882ce9c
Travis Davis <me@tdavis.dev>
2026-01-23 15:47:48 -0600

    feat: Add desktop environment module with Hyprland and Plasma 6 support

---
+++ modules/system/desktops/default.nix
@@ -0,0 +1,79 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.local.desktops;
+  inherit (lib) mkOption mkIf mkForce;
+  inherit (lib.types) bool;
+in {
+  options.local.desktops = {
+    enable = mkOption {
+      type = bool;
+      default = false;
+    };
+    enableEnv = mkOption {
+      type = bool;
+      default = true;
+    };
+    hyprland = mkOption {
+      type = bool;
+      default = false;
+    };
+    niri = mkOption {
+      type = bool;
+      default = false;
+    };
+    plasma6 = mkOption {
+      type = bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Core Desktop Requirements
+    xdg.portal.enable = true;
+    
+    environment.systemPackages = with pkgs; [
+      xdg-user-dirs
+      xdg-utils
+    ];
+
+    # Modern Wayland Environment Variables
+    environment.variables = mkIf cfg.enableEnv {
+      NIXOS_OZONE_WL = "1";
+      CLUTTER_BACKEND = "wayland";
+      GDK_BACKEND = "wayland";
+      GDK_DPI_SCALE = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+      QT_QPA_PLATFORM = "wayland";
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      _JAVA_AWT_WM_NONREPARENTING = "1";
+      _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=on";
+      WARP_ENABLE_WAYLAND = "1";
+    };
+
+    # The Ly Display Manager (Replaces greetd)
+    services.displayManager.ly = {
+      enable = true;
+      settings = {
+        animation = "matrix";
+        restore = true;
+        save = true;
+      };
+    };
+
+    # Desktop Selection logic using inputs from your flake
+    programs.hyprland = mkIf cfg.hyprland {
+      enable = true;
+    };
+
+    # Niri Support
+    programs.niri = mkIf cfg.niri {
+      enable = true;
+    };
+
+    # Plasma 6 Support
+    services.desktopManager.plasma6.enable = cfg.plasma6;
+  };
+}
--- systems/Sapphire/configuration.nix
+++ systems/Sapphire/configuration.nix
@@ -40,43 +40,20 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
 	local = {
+		desktops = {
+			enable = true;
+			enableEnv = true;
+			hyprland = true;
+			plasma6 = true;
+		};
 		userManager.enable = true;
 		repoManager.enable = true;
 		gitSync.enable = true;
