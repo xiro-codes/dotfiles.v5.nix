d51b16cf7019e94a8db6ed0ab1a828dfc537f90b
Travis Davis <me@tdavis.dev>
2026-01-23 15:57:28 -0600

    feat: Add common packages, direnv, and git config

--- home/tod@Sapphire.nix
+++ home/tod@Sapphire.nix
@@ -1,2 +1,29 @@
-{... }: {
+{pkgs, lib, ... }: {
+	home.packages = with pkgs; [
+		unzip
+		p7zip
+		sysstat
+		grim
+		slurp
+		bottom
+		duf
+		dust
+	];
+	programs = {
+		home-manager.enable = true;
+		direnv = {
+			enable = true;
+			nix-direnv.enable = true;
+		};
+		git = {
+			enable = true;
+			userName = "Travis Davis";
+			userEmail = "me@tdavis.dev";
+			extraConfig = {
+				credential.helper = "store";
+				safe.directory = "*";
+
+			};
+		};
+	};
 }
