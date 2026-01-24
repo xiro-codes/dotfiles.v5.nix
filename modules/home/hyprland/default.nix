b6f0de7edf79bcf2f68f586def47371cd17914d7
Travis Davis <me@tdavis.dev>
2026-01-24 09:26:17 -0600

    feat: Add Hyprland module and system variables module

---
+++ modules/home/hyprland/default.nix
@@ -0,0 +1,94 @@
+{ pkgs, config, lib,...}: let 
+  cfg = config.local.hyprland;
+  variables = config.local.variables;
+in {
+  options.local.hyprland = {
+    enable = lib.mkEnableOption "Functional Hyprland setup.";
+  };
+  config = lib.mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      xwayland.enable = true;
+      settings = {
+        monitor = [
+          ",preferred,auto,1"
+        ];
+        input = {
+          kb_layout = "us";
+          follow_mouse = 1;
+          sensitivity = 0;
+          touchpad.natural_scroll = false;
+        };
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+          border_size = 2;
+          layout = "master";
+        };
+        decoration = {
+          rounding = 10;
+          blur = {
+            enable = true;
+            size = 3;
+            passes = 1;
+          };
+          drop_shadow = true;
+          shadow_range = 4;
+          shadow_render_power = 3;
+        };
+                bind = [
+          "$mod, Return, exec, ${variables.terminal}"
+          # "$mod_SHIFT, Return, exec, ${variables.GUI_TERMINAL}"
+
+          "$mod, E, exec, ${variables.guiFileManager}"
+          "$mod_SHIFT, E, exec, ${variables.fileManager}"
+
+          "$mod, P, exec, ${variables.launcher} -show drun -show-icons"
+          # "$mod, T, exec, hdhomerun-client"
+          "$mod, Space, layoutmsg, swapwithmaster master"
+
+          "$mod_SHIFT, Q, killactive"
+
+          # "$mod, Backspace, exec, ${hyprland-scripts}/bin/wm-show_dash"
+
+          "$mod, F, fullscreen"
+          "$mod_SHIFT, F, togglefloating"
+
+          "$mod, H, movefocus, l"
+          "$mod, J, movefocus, d"
+          "$mod, K, movefocus, u"
+          "$mod, L, movefocus, r"
+
+          "$mod_SHIFT, H, movewindow, l"
+          "$mod_SHIFT, J, movewindow, d"
+          "$mod_SHIFT, K, movewindow, u"
+          "$mod_SHIFT, L, movewindow, r"
+
+          "$mod, Tab, changegroupactive"
+          "$mod, G, togglegroup"
+
+          "$mod, U, workspace, 1"
+          "$mod, I, workspace, 2"
+          "$mod, O, workspace, 3"
+
+          "$mod_SHIFT, U, movetoworkspace, 1"
+          "$mod_SHIFT, I, movetoworkspace, 2"
+          "$mod_SHIFT, O, movetoworkspace, 3"
+
+          "$mod, M, workspace, 4"
+          "$mod, comma, workspace, 5"
+          "$mod, period, workspace, 6"
+
+          "$mod_SHIFT, M, movetoworkspace, 4"
+          "$mod_SHIFT, comma,   movetoworkspace, 5"
+          "$mod_SHIFT, period, movetoworkspace, 6"
+        ];
+        bindm = [
+          "$mod,mouse:272, movewindow"
+          "$mod,mouse:273, resizewindow"
+        ];
+
+      };
+    };
+  };
+}
---
+++ modules/home/variables/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, lib, ...}: let 
+  cfg = config.local.variables;
+  mkStrOpt = (default: lib.mkOption { type = lib.types.str; default = default; });
+in {
+  options.local.variables = {
+    enable = lib.mkEnableOption "System variables";
+    editor = mkStrOpt "nvim";
+    guiEditor = mkStrOpt "neovide";
+    fileManager = mkStrOpt "ranger";
+    guiFileManager = mkStrOpt "pcmanfm";
+    terminal = mkStrOpt "kitty";
+    launcher = mkStrOpt "rofi -show drun";
+  };
+  config = lib.mkIf cfg.enable {
+    home.sessionVariables = {
+      EDITOR = cfg.editor;
+      VISUAL = cfg.editor;
+      GUI_EDITOR = cfg.guiEditor;
+      FILEMANAGER = cfg.fileManager;
+      GUI_FILEMANAGER = cfg.guiFIleManager;
+      TERMINAL = cfg.terminal;
+      GUI_TERMINAL = cfg.terminal;
+      LAUNCHER = cfg.launcher;
+    };
+  };
+}
