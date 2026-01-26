910f820d766f9b32de36a52315765b1e972ec320
Travis Davis <me@tdavis.dev>
2026-01-26 12:15:34 -0600

    feat: Add caelestia scheme and update fish config

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -1,34 +1,36 @@
-{config, lib, pkgs, osConfig ? {}, ... }:
-let 
-	isDefaultShell = (osConfig.users.users.${config.home.username}.shell or null) == pkgs.fish;
-in {
-	options.local.fish = {
-		enable = lib.mkOption {
-			type = lib.types.bool;
-			default = isDefaultShell;
-			description = "Enable fish config if it is the system shell.";
-		};
-	};
-	config = lib.mkIf config.local.fish.enable {
-		programs.eza.enable = true;
-		programs.zoxide.enable = true;	
+{ config, lib, pkgs, osConfig ? { }, ... }:
+let
+  isDefaultShell = (osConfig.users.users.${config.home.username}.shell or null) == pkgs.fish;
+in
+{
+  options.local.fish = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = isDefaultShell;
+      description = "Enable fish config if it is the system shell.";
+    };
+  };
+  config = lib.mkIf config.local.fish.enable {
+    programs.eza.enable = true;
+    programs.zoxide.enable = true;
 
 
-		programs.fish = {
-			enable = true;
-			interactiveShellInit = ''
-				set -g fish_greeting ""
-				zoxide init fish | source
-			'';
-			shellAbbrs = {
-				ls = "eza --icons always";
-				cd = "z";
-				lsa = "eza --icons always --all";
-				lsl = "eza --icons always -al";
-				du = "dust";
-				df = "duf";
+    programs.fish = {
+      enable = true;
+      interactiveShellInit = ''
+        				set -g fish_greeting ""
+        				zoxide init fish | source
+                caelestia scheme set -n dynamic
+        			'';
+      shellAbbrs = {
+        ls = "eza --icons always";
+        cd = "z";
+        lsa = "eza --icons always --all";
+        lsl = "eza --icons always -al";
+        du = "dust";
+        df = "duf";
 
-			};
-		};
-	};
+      };
+    };
+  };
 }
