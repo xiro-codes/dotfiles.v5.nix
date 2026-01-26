cd5837ee3121377101f7e393de4e5b0492ea9cf0
Travis Davis <me@tdavis.dev>
2026-01-26 10:55:45 -0600

    feat: mpd: Enable pipewire output, mpd-mpris, and ncmpcpp visualizer support

--- modules/home/mpd/default.nix
+++ modules/home/mpd/default.nix
@@ -1,48 +1,51 @@
-{pkgs, config, lib, ...}: let 
-cfg = config.local.mpd;
-in {
+{ pkgs, config, lib, ... }:
+let
+  cfg = config.local.mpd;
+in
+{
   options.local.mpd = {
     enable = lib.mkEnableOption "Enable mpd";
-    path = lib.mkOption { type = lib.types.str; default = "/mnt/zima/Music";};
+    path = lib.mkOption { type = lib.types.str; default = "/mnt/zima/Music"; };
   };
   config = lib.mkIf cfg.enable {
-    home.packages = [pkgs.mpc];
-    services.mpd =  {
+    home.packages = [ pkgs.mpc ];
+    services.mpd-mpris.enable = true;
+    services.mpd = {
       enable = true;
       musicDirectory = cfg.path;
       extraConfig = ''
+          audio_output {
+            type "pipewire"
+              name "Pipewire"
+          }
         audio_output {
-          type "pipewire"
-            name "Pipewire"
+          type "fifo"
+            name "Visualizer"
+            path "/tmp/mpd.fifo"
+            format "44100:16:2"
         }
-      audio_output {
-        type "fifo"
-          name "Visualizer"
-          path "/tmp/mpd.fifo"
-          format "44100:16:2"
-      }
       '';
     };
     programs.ncmpcpp = {
       enable = true;
-      package = pkgs.ncmpcpp.override {visualizerSupport = true;};
+      package = pkgs.ncmpcpp.override { visualizerSupport = true; };
       bindings = [
-      {
-        key = "j";
-        command = "scroll_down";
-      }
-      {
-        key = "k";
-        command = "scroll_up";
-      }
-      {
-        key = "J";
-        command = ["select_item" "scroll_down"];
-      }
-      {
-        key = "K";
-        command = ["select_item" "scroll_up"];
-      }
+        {
+          key = "j";
+          command = "scroll_down";
+        }
+        {
+          key = "k";
+          command = "scroll_up";
+        }
+        {
+          key = "J";
+          command = [ "select_item" "scroll_down" ];
+        }
+        {
+          key = "K";
+          command = [ "select_item" "scroll_up" ];
+        }
       ];
       settings = {
         execute_on_song_change = ''notify-send -i /tmp/.music_cover.png "Playing" "$(mpc --format '%title% - %album%' current)"'';
