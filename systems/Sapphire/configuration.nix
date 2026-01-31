95a6174903d00f1bb834c303ab6c81bea3935313
Travis Davis <me@tdavis.dev>
2026-01-30 20:45:24 -0600

    feat: Enable local caching and add secrets keys for gemini/api_key and zima_creds.

--- systems/Sapphire/configuration.nix
+++ systems/Sapphire/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -9,6 +5,7 @@
 
   local = {
     cache.enable = true;
+    secrets.keys = [ "gemini/api_key" "zima_creds" ];
 
     bootloader = {
       mode = "uefi";
