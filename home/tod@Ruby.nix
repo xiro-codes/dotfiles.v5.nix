ce8e828fe8fcbefcdff3fcb64e72c13c39111663
Travis Davis <me@tdavis.dev>
2026-01-31 21:52:53 -0600

    feat: Add avante plugin for gemini code generation

--- home/tod@Ruby.nix
+++ home/tod@Ruby.nix
@@ -1,7 +1,10 @@
-{ pkgs, inputs, lib, ... }: {
+{ pkgs, inputs, lib, config,... }: {
   imports = [
     ./profiles/workstation.nix
   ];
   local.secrets.keys = [ "gemini/api_key" ];
+  home.sessionVariables = {
+    GEMINI_API_KEY = "$(cat ${config.sops.secrets."gemini/api_key".path})";
+  };
   home.stateVersion = "25.11";
 }
