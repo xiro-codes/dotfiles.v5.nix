59b29404aed751549368f49a1164074aaa3db1a0
Travis Davis <me@tdavis.dev>
2026-02-02 21:22:15 -0600

    feat: Remove nixos-generators input and installer-iso package generation.

--- flake.nix
+++ flake.nix
@@ -23,10 +23,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     deploy-rs.url = "github:serokell/deploy-rs";
     disko = {
       url = "github:nix-community/disko/latest";
@@ -63,54 +59,8 @@
       perSystem =
         { config, pkgs, system, ... }:
         {
-
           devShells = {
             default = pkgs.callPackage ./shells/default/default.nix { inherit pkgs inputs; };
-          };
-          packages.installer-iso = inputs.nixos-generators.nixosGenerate {
-            system = "x86_64-linux";
-            format = "install-iso";
-            specialArgs = { inherit inputs; };
-            modules = [
-              inputs.self.nixosModules.cache
-              inputs.self.nixosModules.settings
-              {
-                local = {
-                  cache.enable = true;
-                  settings.enable = true;
-                };
-                imports = [
-                  "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-                ];
-                users.motd = ''
-                  Custom NixOS Installer
-                  1. git clone http://10.0.0.65:3002/xiro/dotfiles.nix.git
-                  2. cd dotfiles.nix
-                  3. nix develop
-                  4. just install HOSTNAME
-                '';
-
-                environment.etc."dotfiles-src".source = builtins.path {
-                  path = inputs.self.outPath;
-                  name = "dotfiles-git-src";
-                  filter = path: type: true;
-                };
-                environment.systemPackages =
-                  with pkgs;
-                  [
-                    python3
-                    git
-                    parted
-                    util-linux
-                  ]
-                  ++ [
-                    inputs.self.packages.x86_64-linux.install-system
-                  ];
-                nixpkgs.config.allowUnfree = true;
-
-                networking.hostName = "installer";
-              }
-            ];
           };
         };
     };
