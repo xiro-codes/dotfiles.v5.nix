99dcf0e06fd0249bf5e865bb75e32c35e38e7afa
Travis Davis <me@tdavis.dev>
2026-02-04 06:39:40 -0600

    fix: Use `lib.nameValuePair` in reverse proxy module to preserve service path names.

--- modules/system/reverse-proxy/default.nix
+++ modules/system/reverse-proxy/default.nix
@@ -114,24 +114,26 @@ in
               add_header Content-Type text/plain;
             '';
           };
-        } // lib.mapAttrs (name: service: {
-          proxyPass = service.target;
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Host $host;
-            proxy_set_header X-Forwarded-Server $host;
-            
-            # WebSocket support
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-            
-            ${service.extraConfig}
-          '';
-        }) cfg.services;
+        } // lib.mapAttrs' (name: service: 
+          lib.nameValuePair service.path {
+            proxyPass = service.target;
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header X-Forwarded-Host $host;
+              proxy_set_header X-Forwarded-Server $host;
+              
+              # WebSocket support
+              proxy_http_version 1.1;
+              proxy_set_header Upgrade $http_upgrade;
+              proxy_set_header Connection "upgrade";
+              
+              ${service.extraConfig}
+            '';
+          }
+        ) cfg.services;
       };
     };
 
