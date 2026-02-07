4ee11dcb2f18e08a4913aeddc9d759e4f8be901e
Travis Davis <me@tdavis.dev>
2026-02-07 14:14:12 -0600

    feat: Enable Flatpak support with Flathub and Bazaar.

---
+++ modules/system/flatpak/default.nix
@@ -0,0 +1,30 @@
+{ lib, pkgs, config, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.system.flatpak;
+in
+{
+  options.modules.system.flatpak = {
+    enable = mkEnableOption "Flatpak support";
+  };
+
+  config = mkIf cfg.enable {
+    services.flatpak.enable = true;
+
+    system.fsPackages = [ pkgs.flatpak ];
+
+    # Flathub repository
+    services.flatpak.remotes = [{
+      name = "flathub";
+      location = "https://flathub.org/repo/flathub.flatpakrepo";
+    }];
+
+    # System-wide installation of applications
+    environment.flatpak.packages = [
+      # A modern software store for GNOME
+      "org.bazaar.Bazaar"
+    ];
+  };
+}
