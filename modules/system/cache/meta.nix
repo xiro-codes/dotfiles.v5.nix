5a8261537c3b8dacf3ab4acf954803a483145945
Travis Davis <me@tdavis.dev>
2026-02-07 20:19:42 -0600

    fix: Mark nix cache modules as broken and include Bazaar flatpak by default.

--- modules/system/cache/meta.nix
+++ modules/system/cache/meta.nix
@@ -1,5 +1,5 @@
 {
-  broken = false;
+  broken = true;
   description = "Configures the system to use a binary cache for Nix packages.";
   tags = [ "nix" "cache" "client" "performance" ];
   maintainers = [ "tod" ];
