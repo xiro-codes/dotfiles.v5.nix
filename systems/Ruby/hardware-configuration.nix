887e1b688ef9a5da7ebefab5a77a34b6056fed91
Travis Davis <me@tdavis.dev>
2026-02-08 17:58:04 -0600

    feat: Add hid-playstation kernel module for controller support

--- systems/Ruby/hardware-configuration.nix
+++ systems/Ruby/hardware-configuration.nix
@@ -11,21 +11,8 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "hid-playstation" ];
   boot.extraModulePackages = [ ];
-
-  #  fileSystems."/" =
-  #    { device = "/dev/disk/by-uuid/d55b8028-f948-429b-abf9-c733fb454039";
-  #      fsType = "ext4";
-  #    };
-  #
-  #  fileSystems."/boot" =
-  #    { device = "/dev/disk/by-uuid/6CBA-366A";
-  #      fsType = "vfat";
-  #      options = [ "fmask=0022" "dmask=0022" ];
-  #    };
-  #
-  #  swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
