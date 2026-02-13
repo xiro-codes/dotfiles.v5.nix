e4d3138adaf2cfde1af16bcf0ae8a4ee0fdd3c52
Travis Davis <me@tdavis.dev>
2026-02-13 03:58:36 -0600

    feat: Adjust Hyprland window opacity and disable blur.

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -67,11 +67,11 @@ in
         };
         decoration = {
           rounding = 20;
-          active_opacity = "0.9";
-          inactive_opacity = "0.75";
+          active_opacity = "1.0";
+          inactive_opacity = "0.95";
           fullscreen_opacity = "1.0";
           blur = {
-            enabled = true;
+            enabled = false;
           };
         };
         binds = {
