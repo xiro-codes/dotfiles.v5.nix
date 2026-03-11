43441d52aea997b9aedaf981d6861fb7625678e3
Travis Davis <me@tdavis.dev>
2026-03-10 19:52:26 -0500

    feat: Configure starship prompt with custom format and integrations.

--- home/tod@Ruby.nix
+++ home/tod@Ruby.nix
@@ -6,5 +6,65 @@
   home.packages = with pkgs; [
     godot
   ];
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      add_newline = false;
+      line_break.disabled = true;
+      format = ''
+        [\[I\] ](white)$username@$hostname $directory$git_branch$nix_shell$rust$python$character
+      '';
+
+      # 2. Replicate the [I] and user@host style
+      username = {
+        show_always = true;
+        format = "[$user]($style)";
+        style_user = "white";
+      };
+
+      hostname = {
+        ssh_only = false;
+        format = "[$hostname]($style) ";
+        style = "white";
+      };
+
+      directory = {
+        style = "blue";
+        truncation_length = 3;
+        fish_style_pwd_dir_length = 1; # Replicates ~/P/style
+      };
+
+      git_branch = {
+        symbol = ""; # Remove the default icon if you want it exactly like std fish
+        format = "([$branch]($style)) ";
+        style = "white";
+      };
+
+      # 3. Additions for Nix, Rust, and Python
+      nix_shell = {
+        symbol = "❄️ ";
+        format = "via [$symbol$state]($style) ";
+        style = "bold blue";
+      };
+
+      rust = {
+        symbol = "🦀 ";
+        format = "[$symbol$version]($style) ";
+        style = "bold red";
+      };
+
+      python = {
+        symbol = "🐍 ";
+        format = "[$symbol$version]($style) ";
+        style = "yellow";
+      };
+
+      character = {
+        success_symbol = "[>](bold white)";
+        error_symbol = "[>](bold red)";
+      };
+    };
+  };
   home.stateVersion = "25.11";
 }
