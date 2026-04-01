afa9b54293f77722c22a0744336a9999887c7bd5
Travis Davis <me@tdavis.dev>
2026-04-01 02:54:29 -0500

    fix: Update zen-browser flake source and remove pulseaudio fix.

--- systems/profiles/workstation/software.nix
+++ systems/profiles/workstation/software.nix
@@ -1,17 +1,7 @@
 { inputs, pkgs, ... }:
-let
-  zen-browser-fixed = (inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default).overrideAttrs (oldAttrs: {
-    buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.libpulseaudio ];
-    preFixup = (oldAttrs.preFixup or "") + ''
-      gappsWrapperArgs+=(
-        --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.libpulseaudio ]}"
-      )
-    '';
-  });
-in
 {
   environment.systemPackages = [
-    zen-browser-fixed
+    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
   programs.kdeconnect.enable = true;
 }
