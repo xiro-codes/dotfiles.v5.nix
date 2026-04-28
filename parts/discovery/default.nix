49a519f5ae4a6d44ddd19fc48e35e251d2e38f74
Travis Davis <me@tdavis.dev>
2026-04-28 04:00:52 -0500

    build(discovery): disable default overlay

--- parts/discovery/default.nix
+++ parts/discovery/default.nix
@@ -64,7 +64,7 @@ in
     nixosContainers = nixosLib.containers;
     homeConfigurations = homeLib.mkHomeConfigurations;
     templates = discoveredTemplates;
-    overlays.default = final: prev: packagesLib.mkPackages paths.packages final.stdenv.hostPlatform.system;
+    #overlays.default = final: prev: packagesLib.mkPackages paths.packages final.stdenv.hostPlatform.system;
     deploy.nodes = deployLib.mkDeployNodes;
   };
 }
