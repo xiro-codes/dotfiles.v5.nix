77fb48973917f09641d56b1ce41178d3008a6b05
Travis Davis <me@tdavis.dev>
2026-04-28 03:45:54 -0500

    refactor(containers): move system definitions
    Relocate container system configurations to `systems/containers/` and
    introduce a new module for automated management. Removes Cinnabar system.

---
+++ modules/system/containers/meta.nix
@@ -0,0 +1,6 @@
+{
+  broken = false;
+  description = "Automated management of NixOS containers defined in systems/containers/";
+  tags = [ "containers" "system" "automation" ];
+  maintainers = [ "tod" ];
+}
