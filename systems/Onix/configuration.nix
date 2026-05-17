ae65c545ae81143b8fd04a4ef769fc75ec4f6d7a
Travis Davis <me@tdavis.dev>
2026-05-17 05:26:18 -0500

    chore(Onix): disable Incus virtualization

--- systems/Onix/configuration.nix
+++ systems/Onix/configuration.nix
@@ -25,7 +25,7 @@
     ];
     zerotier.enable = true;
     containers.Jade.enable = true;
-    virtualisation.incus.enable = true;
+    virtualisation.incus.enable = false;
     metrics = {
       enable = true;
       domain = "pihole.onix.home";
