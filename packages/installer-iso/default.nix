d6e93949b001715ff221079f3b9b36e67819e147
Travis Davis <me@tdavis.dev>
2026-05-18 09:15:57 -0500

    feat(installer-iso): configure remote builder
    Adds Nix distributed build configuration for 'ruby' host and
    includes SSH config and MOTD instructions for setup.

--- packages/installer-iso/default.nix
+++ packages/installer-iso/default.nix
@@ -15,45 +15,87 @@
 
     self.nixosModules.network-hosts
     inputs-nix.nixosModules.default
-    {
-      local = {
-        #cache.enable = true;
-        nix-core-settings.enable = true;
-        nix-cache-client.enable = true;
+    (
+      { config, ... }:
+      {
+        local = {
+          #cache.enable = true;
+          nix-core-settings.enable = true;
+          nix-cache-client.enable = true;
 
-      };
-      imports = [
-        "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-      ];
-      boot.zfs.forceImportRoot = false;
-      boot.tmp.tmpfsSize = "8G";
-      fileSystems."/".options = [ "size=8G" ];
-      environment.systemPackages = [
-        inputs.nixpkgs.legacyPackages.x86_64-linux.python3
-        inputs.nixpkgs.legacyPackages.x86_64-linux.git
-        inputs.nixpkgs.legacyPackages.x86_64-linux.parted
-        inputs.nixpkgs.legacyPackages.x86_64-linux.util-linux
-        inputs.nixpkgs.legacyPackages.x86_64-linux.fastfetch
-      ];
-
-      fonts = {
-        enableDefaultPackages = true;
-        packages = with inputs.nixpkgs.legacyPackages.x86_64-linux; [
-          noto-fonts
-          noto-fonts-emoji
-          noto-fonts-cjk-sans
+        };
+        imports = [
+          "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
         ];
-      };
-      nixpkgs.config.allowUnfree = true;
+        boot.zfs.forceImportRoot = false;
+        boot.tmp.tmpfsSize = "8G";
+        fileSystems."/".options = [ "size=8G" ];
+        environment.systemPackages = [
+          inputs.nixpkgs.legacyPackages.x86_64-linux.python3
+          inputs.nixpkgs.legacyPackages.x86_64-linux.git
+          inputs.nixpkgs.legacyPackages.x86_64-linux.parted
+          inputs.nixpkgs.legacyPackages.x86_64-linux.util-linux
+          inputs.nixpkgs.legacyPackages.x86_64-linux.fastfetch
+        ];
 
-      services.openssh = {
-        enable = true;
-        settings.PasswordAuthentication = true;
-        settings.PermitRootLogin = "yes";
-      };
+        fonts = {
+          enableDefaultPackages = true;
+          packages = with inputs.nixpkgs.legacyPackages.x86_64-linux; [
+            noto-fonts
+            noto-fonts-emoji
+            noto-fonts-cjk-sans
+          ];
+        };
+        nixpkgs.config.allowUnfree = true;
 
-      networking.hostName = "installer";
+        services.openssh = {
+          enable = true;
+          settings.PasswordAuthentication = true;
+          settings.PermitRootLogin = "yes";
+        };
 
-    }
+        users.motd = ''
+          =======================================================================
+          Welcome to the NixOS Installer!
+
+          To use the 'ruby' remote builder for fast builds:
+          Run this command on the ruby host to copy its key over to this ISO:
+          `scp /root/.ssh/id_ed25519 root@<ISO_IP>:/root/.ssh/id_rsa_builder`
+          (Find this ISO's IP by running `ip a`)
+
+          The remote builder will then be automatically used for Nix commands!
+          =======================================================================
+        '';
+
+        programs.ssh.extraConfig = ''
+          Host ${config.local.network-hosts.ruby} ruby
+            StrictHostKeyChecking no
+            UserKnownHostsFile /dev/null
+        '';
+
+        nix = {
+          distributedBuilds = true;
+          buildMachines = [
+            {
+              hostName = config.local.network-hosts.ruby;
+              system = "x86_64-linux";
+              protocol = "ssh-ng";
+              maxJobs = 8;
+              sshKey = "/root/.ssh/id_rsa_builder";
+              sshUser = "root";
+              supportedFeatures = [
+                "nixos-test"
+                "benchmark"
+                "big-parallel"
+                "kvm"
+              ];
+            }
+          ];
+        };
+
+        networking.hostName = "installer";
+
+      }
+    )
   ];
 }).config.system.build.isoImage
