5f77f3164bef5a9272279ec02b893de1881859dd
Travis Davis <me@tdavis.dev>
2026-05-21 06:03:29 -0500

    feat(kitty): add Neovim-like split navigation

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -18,6 +18,22 @@ in
       enable = true;
       extraConfig = ''
         window_padding_width 5
+
+        # Neovim-like splits and pane navigation
+        enabled_layouts splits
+
+        # Create splits (vsplit = right, hsplit = down)
+        map ctrl+w>v launch --location=vsplit
+        map ctrl+w>s launch --location=hsplit
+
+        # Navigate splits
+        map ctrl+w>h neighboring_window left
+        map ctrl+w>l neighboring_window right
+        map ctrl+w>k neighboring_window up
+        map ctrl+w>j neighboring_window down
+
+        # Close split
+        map ctrl+w>q close_window
       '';
     };
   };
