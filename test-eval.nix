2636cf224e6563ae264ebda8318fcd3f00187b55
Travis Davis <me@tdavis.dev>
2026-05-23 18:14:24 -0500

    chore(nix): update nixpkgs and remove test eval script
    Removes `test-eval.nix` as it is no longer needed.

--- test-eval.nix
+++
@@ -1,9 +0,0 @@
-let
-  flake = builtins.getFlake (toString ./.);
-  inputs = flake.inputs;
-  system = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [ { fileSystems."/".device = "/dev/null"; boot.loader.grub.device = "/dev/null"; } ];
-  };
-in
-  system.config.system.build.toplevel.outPath
