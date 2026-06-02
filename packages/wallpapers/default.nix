a67eb9388d4b3e29620a5b410cd615c9f0671853
Travis Davis <me@tdavis.dev>
2026-06-02 10:56:02 -0500

    chore(wallpapers): update wallpaper source domain

--- packages/wallpapers/default.nix
+++ packages/wallpapers/default.nix
@@ -24,7 +24,7 @@ pkgs.linkFarm "wallpapers" [
     name = "miku.jpeg";
     path = fetchWallpaper {
       name = "miku.jpeg";
-      url = "https://wallpapers.onix.home/miku.jpeg";
+      url = "https://wallpapers.sapphire.home/miku.jpeg";
       sha256 = "sha256-Lp6CAHJc+rJEWDo3z9DtH/J543zdJth079M3nMW1OwM=";
     };
   }
@@ -32,7 +32,7 @@ pkgs.linkFarm "wallpapers" [
     name = "deskmat-1.jpg";
     path = fetchWallpaper {
       name = "deskmat-1.jpg";
-      url = "https://wallpapers.onix.home/Deskmat/1.jpg";
+      url = "https://wallpapers.sapphire.home/Deskmat/1.jpg";
       sha256 = "sha256-MDIjJVlhXCLgCMsc9aGEx8A09hgJasjjvWdTTrTVL5c=";
     };
   }
@@ -40,7 +40,7 @@ pkgs.linkFarm "wallpapers" [
     name = "deskmat-2.jpg";
     path = fetchWallpaper {
       name = "deskmat-2.jpg";
-      url = "https://wallpapers.onix.home/Deskmat/2.jpg";
+      url = "https://wallpapers.sapphire.home/Deskmat/2.jpg";
       sha256 = "sha256-tTo3IwJMansdNaFJvMAWYMcFtrHqPx3fY7ixpA4hv+A=";
     };
   }
