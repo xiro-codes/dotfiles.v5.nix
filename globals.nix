950ac1d65bc881e922d3a9b3050461af1d0a1ac4
Travis Davis <me@tdavis.dev>
2026-06-15 21:53:20 -0500

    feat(nix): add globals.nix for shared modules
    Centralize common NixOS and Home Manager module imports.
    This simplifies system configurations and promotes consistency.

---
+++ globals.nix
@@ -0,0 +1,46 @@
+{
+  inputs,
+  selfPath ? ./.,
+}:
+{
+  globalNixosModules = [
+    { _module.args.flake-inputs = inputs; }
+    {
+      imports = [
+        (selfPath + "/modules/system/core/bootloader/module.nix")
+        (selfPath + "/modules/system/core/disks/module.nix")
+        (selfPath + "/modules/system/networking/network/module.nix")
+        (selfPath + "/modules/system/core/nix-core-settings/module.nix")
+        (selfPath + "/modules/system/core/secrets/module.nix")
+        (selfPath + "/modules/system/admin/security/module.nix")
+        (selfPath + "/modules/system/admin/user-manager/module.nix")
+        (selfPath + "/modules/system/core/localization/module.nix")
+        inputs.disko.nixosModules.disko
+        inputs.sops-nix.nixosModules.sops
+        inputs.home-manager.nixosModules.home-manager
+        inputs.nix-flatpak.nixosModules.nix-flatpak
+        inputs.gog-nix.nixosModules.gog
+        inputs.rocket-blog.nixosModules.default
+        inputs.silentsddm.nixosModules.default
+        inputs.harmonia.nixosModules.harmonia
+        inputs.impermanence.nixosModules.impermanence
+        inputs.determinate.nixosModules.default
+        inputs.nix-topology.nixosModules.default
+        inputs.nix-compose.nixosModules.daemon
+        inputs.nixarr.nixosModules.default
+      ];
+    }
+  ];
+
+  globalHomeModules = [
+    {
+      imports = [
+        inputs.fuchsia-nix.homeModules.default
+        inputs.sops-nix.homeModules.sops
+        inputs.caelestia-shell.homeManagerModules.default
+        inputs.nixvim.homeModules.nixvim
+        inputs.stylix.homeModules.stylix
+      ];
+    }
+  ];
+}
