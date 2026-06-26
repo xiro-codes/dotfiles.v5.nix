d8a918a1029edeeedfab3d0b132c6d470738315a
Travis Davis <me@tdavis.dev>
2026-06-26 12:17:33 -0500

    build(uabea): upgrade to dotnet 8.0 and version 8.0.0
    Upgrade the package to use dotnet 8.0 SDK and runtime, update the
    version to 8.0.0, and switch nugetDeps to deps.json.

---
+++ packages/uabea/default.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  gnumake,
+  buildDotnetModule,
+  fetchFromGitHub,
+  dotnetCorePackages,
+  ...
+}:
+
+buildDotnetModule rec {
+  pname = "uabea";
+  version = "8.0.0";
+
+  src = fetchFromGitHub {
+    owner = "nesrak1";
+    repo = "UABEA";
+    rev = "v8";
+    sha256 = "1p5va3fm48wbqn8fcgx8dbral5lbwxzxdsaf93rdfjznnn8zcpc7";
+  };
+
+  projectFile = "UABEAvalonia.sln";
+  nugetDeps = ./deps.json;
+  
+  dotnet-sdk = dotnetCorePackages.sdk_8_0;
+  dotnet-runtime = dotnetCorePackages.runtime_8_0;
+
+  nativeBuildInputs = [
+    stdenv.cc
+    gnumake
+  ];
+
+  postPatch = ''
+    find . -type f -name "*.csproj" -exec sed -i 's/net6.0/net8.0/g' {} +
+    dotnet sln ''${projectFile} remove TexToolWrap/TexToolWrap.vcxproj
+  '';
+
+  executables = [ "UABEAvalonia" ];
+
+  meta = with lib; {
+    description = "Unity Asset Bundle Extractor Avalon";
+    homepage = "https://github.com/nesrak1/UABEA";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+}
