e1734400a5554a1c45dd1a538ca5cd153c0bc306
Travis Davis <me@tdavis.dev>
2026-06-29 05:53:19 -0500

    feat(home/ruby): add xiro configuration

---
+++ homes/Ruby/xiro.nix
@@ -0,0 +1,25 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: let in {
+  imports = [ 
+    ../profiles/workstation
+    ../profiles/nsfw
+  ]
+  home.packages = with pkgs; [
+    vlc
+  ];
+  services.espanso = {
+    package = pkgs..espanso-wayland;
+    matches.base.matches = [
+      {
+        trigger = "dto";
+        replace = "dot";
+        word = true;
+      }
+    ];
+  };
+  home.stateVersion = "26.05";
+}
