480f65a8be434fcb8d03434e96b9f8e1cebc58d7
Travis Davis <me@tdavis.dev>
2026-06-29 20:56:17 -0500

    style(desktop): disable stylix and configure Cascadia Code font

--- homes/profiles/desktop/appearance.nix
+++ homes/profiles/desktop/appearance.nix
@@ -8,17 +8,27 @@ let
 in
 {
   local = {
-    stylix.enable = true;
+    stylix.enable = false;
     fonts.enable = true;
   };
 
   gtk = {
     enable = mkForce true;
     gtk4.theme = mkForce null;
+    font = {
+      name = "Cascadia Code";
+      package = pkgs.cascadia-code;
+      size = 11;
+    };
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+  };
+
+  programs.kitty.font = {
+    name = "Cascadia Code";
+    size = 10;
   };
 
   home.packages = with pkgs; [
